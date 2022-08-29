9dea1430c96563cf727ba449463704aa4e0a6538
nevanalphonso@gmail.com <nevanalphonso.com>
2022-08-29 09:52:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Login from "./pages/Login";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import { Card, Button, Tooltip } from "antd";
+import React from "react";
+import "antd/dist/antd.css";
+import "../styles/Login.css";
+
+import Background from "../assets/images/floor-background.webp";
+import TeknorixLogo from "../assets/images/teknorix-logo.svg";
+import GoogleLogo from "../assets/images/icons8-google.svg";
+import MicrosoftLogo from "../assets/images/icons8-microsoft.svg";
+
+const Login = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "repeat",
+        width: "100vw",
+        height: "100vh",
+        // minWidth: "100%",
+        // minHeight: "100%",
+        overflow: "hidden",
+        backgroundColor: "#aed3eb",
+        backgroundBlendMode: "multiply",
+        // display: "flex",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          // flexDirection: "row",
+          // alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Card className="card">
+          <img className="teknorix-logo" src={TeknorixLogo}></img>
+          <p className="login-text">Log in to DeskBooker</p>
+
+          <Button className="Login-button" block>
+            <span style={{ marginRight: 10 }}>
+              <img src={GoogleLogo}></img>
+            </span>
+            <span>Continue with Google</span>
+          </Button>
+
+          <Button className="Login-button" block>
+            <span style={{ marginRight: 10 }}>
+              <img src={MicrosoftLogo}></img>
+            </span>
+            <span>Continue with Microsoft</span>
+          </Button>
+        </Card>
+
+        {/* <div
+          className="slogan"
+          style={{
+            marginTop: "10%",
+            marginRight: "10%",
+            width: "200px",
+          }}
+        > */}
+        <h1 className="slogan" style={{ color: "white" }}>
+          BOOK YOUR DESK NOW
+        </h1>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
